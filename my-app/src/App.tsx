8b99d15b9ea836f32eb77804539c1c1a2134183f
ValaA88t <vala.arefi@gmail.com>
2021-09-02 09:36:53 +0200

    Adding img

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -86,6 +86,14 @@ function App() {
           <a href="/login">Login</a>
         </button>
       </div>
+      <div>
+        <img
+          src="backgroundLogin.png"
+          key="backgroundLogin"
+          alt="backgroundLogin"
+          className="image"
+        ></img>
+      </div>
     </form>
   );
 }
